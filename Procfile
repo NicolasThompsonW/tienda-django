aeda85113876cc11b17a553ba882905f514c121e
NicolasThompson <nicolasthompsonweb@gmail.com>
2023-02-02 15:10:00 -0300

    2

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: python manage.py migrate && gunicorn tiendaWeb.wsgi
+environment:
+  STATIC_ROOT: /app/my_project/staticfiles
--- tiendaWeb/settings.py
+++ tiendaWeb/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
