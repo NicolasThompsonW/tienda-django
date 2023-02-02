2ef1ff1744db94682e5c3cfea48cbd1e4f025f83
NicolasThompson <nicolasthompsonweb@gmail.com>
2023-02-02 19:47:16 -0300

    meddia

--- tiendaWeb/settings.py
+++ tiendaWeb/settings.py
@@ -167,7 +167,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
