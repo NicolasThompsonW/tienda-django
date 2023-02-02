c75bf85df87c75165e9fbdec87cd5db4c367ef27
NicolasThompson <nicolasthompsonweb@gmail.com>
2023-02-02 00:31:13 -0300

    deploy4

--- tiendaWeb/settings.py
+++ tiendaWeb/settings.py
@@ -91,12 +91,15 @@ WSGI_APPLICATION = "tiendaWeb.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '3KOPTqL2ZlSc76uHDvnm',
+        'HOST': 'containers-us-west-114.railway.app',
+        'PORT': '7231',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
